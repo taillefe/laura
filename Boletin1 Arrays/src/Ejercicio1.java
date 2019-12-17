58a95c99935cf6b3e0f4f60c3a5a02d359ab6e08
FP <FP@PC22>
2019-12-17 13:44:15 +0100

    .
    Signed-off-by: FP <FP@PC22>

---
+++ Boletin1 Arrays/src/Ejercicio1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+	
+		String frase;
+		char[] lista; 
+		
+		frase = obtenerTexto();
+		lista = obtenerArray(frase);
+		
+
+	}
+
+
+
+
+	public static String obtenerTexto()
+	{
+		System.out.println("Introduce una frase");
+		Scanner sc = new Scanner(System.in);
+		String texto = sc.nextLine();
+		return texto;
+	}
+	
+	public static char[] obtenerArray(String t);
+	{
+		
+		
+		return 
+	}
+}
